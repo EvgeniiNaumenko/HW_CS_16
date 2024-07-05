f3dce8a29594e87326b5d3f3733e1349c08b1c28
Naumenko Evgenii <evgeniynaumenko2017@gmail.com>
2024-07-05 18:01:46 +0300

    first commit

---
+++ Magazine.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+//    Створіть програму для роботи з інформацією про журнал, в якій
+//зберігатиметься таку інформація:
+//1. Назва журналу.+
+//2. Назва видавництва.+
+//3. Дата видання.+
+//4. Кількість сторінок.+
+//Програма має бути з такою функціональністю:
+//1. Введення інформації про журнал.+
+//2. Виведення інформації про журнал.+
+//3. Серіалізація журналу.
+//4. Збереження серіалізованого журналу у файл.
+//5. Завантаження серіалізованого журналу з файлу.Після
+//завантаження потрібно виконати десеріалізацію журналу.
+//Вибір певного формату серіалізації потрібно зробити вам.
+//Звертаємо вашу увагу, що вибір має бути обґрунтованим.
+namespace HW_16_01_07_2024
+{
+    [Serializable]
+    internal class Magazine
+    {
+        public string Name {  get; set; }
+        public string PublHouse {  get; set; }
+        public DateTime Date { get; set; }
+        public int NumPages { get; set; }
+        public Magazine() { }
+        public Magazine(string name, string publHouse, DateTime date, int numPages)
+        {
+            Name = name;
+            PublHouse = publHouse;
+            Date = date;
+            NumPages = numPages;
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}\nPublishing house: {PublHouse}\nPublishing date: {Date}\nNumber of pages: {NumPages}";
+        }
+        public void setInfo (string name, string publHouse, DateTime date, int numPages)
+        {
+            Name = name ;
+            PublHouse = publHouse ;
+            Date = date ;
+            NumPages = numPages ;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+namespace HW_16_01_07_2024
+{
+//    Створіть програму для роботи з інформацією про журнал, в якій
+//зберігатиметься таку інформація:
+//1. Назва журналу.
+//2. Назва видавництва.
+//3. Дата видання.
+//4. Кількість сторінок.
+//Програма має бути з такою функціональністю:
+//1. Введення інформації про журнал.
+//2. Виведення інформації про журнал.
+//3. Серіалізація журналу.
+//4. Збереження серіалізованого журналу у файл.
+//5. Завантаження серіалізованого журналу з файлу.Після
+//завантаження потрібно виконати десеріалізацію журналу.
+//Вибір певного формату серіалізації потрібно зробити вам.
+//Звертаємо вашу увагу, що вибір має бути обґрунтованим.
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathBIN = @".\data.bin";
+            string pathXML = @".\data.xml";
+            string pathJSON = @".\data.json";
+            Magazine magazine = new Magazine("Science and Technology", "Knowledge Publishing", new DateTime(2024,07,01), 128);
+            MySerializer mySerializer = new MySerializer();
+            Console.WriteLine("=====BIN=====");
+            mySerializer.SerializeBIN(magazine, pathBIN);
+            Magazine magazine1 = mySerializer.DeSerializeBIN(pathBIN);
+            Console.WriteLine(magazine1);
+            Console.WriteLine("=====JSON=====");
+            mySerializer.SerializeJSON(magazine, pathJSON);
+            Magazine magazine2 = mySerializer.DeSerializeJSON(pathJSON);
+            Console.WriteLine(magazine2);
+            Console.WriteLine("=====XML=====");
+            mySerializer.SerializeXML(magazine, pathXML);
+            Magazine magazine3 = mySerializer.DeSerializeXML(pathXML);
+            Console.WriteLine(magazine3);
+        }
+    }
+}
